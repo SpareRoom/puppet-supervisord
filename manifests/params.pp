16a2553b16b79ca72012f27f23b68905a27ecc94
Mike Raynham <enquiries@mikeraynham.co.uk>
2015-07-03 09:56:20 +0100

    Fix typos

--- manifests/params.pp
+++ manifests/params.pp
@@ -70,8 +70,8 @@ class supervisord::params {
   $inet_server_hostname = '127.0.0.1'
   $inet_server_port     = '9001'
   $inet_auth            = false
-  $inet_username        = undef,
-  $inet_password        = undef,
+  $inet_username        = undef
+  $inet_password        = undef
 
   $init_template        = "supervisord/init/${::osfamily}/defaults.erb"
 }
